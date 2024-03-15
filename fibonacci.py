eca926ab07f6e2e5638aa1d1d7c249a055704a41
Yury Ivashka <59415902+implementatorr@users.noreply.github.com>
2024-03-15 09:13:18 +0100

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+
+
+def fibonacci_of(n):
+    if n in {0, 1}:  # Base case
+        return n
+    return fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
+
+
+inp = int(input("Enter a number :"))
+print(fibonacci_of(inp))
